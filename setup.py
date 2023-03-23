82cce883022d0c9901c420328e9370fd457b6989
Khari Gardner <kharigardner@protonmail.com>
2023-03-23 02:55:29 -0400

    removed read me from setup completely

--- setup.py
+++ setup.py
@@ -1,8 +1,5 @@
 from setuptools import setup
 import os
-this_directory = os.path.dirname(__file__)
-readme_file = os.path.join(this_directory, 'readme.md')
-readme = open(readme_file, encoding='utf-8').read()
 
 setup(
     name='pycallrail',
