94f25fb476d9d10406132f6d4280fa9f864258e8
Khari Gardner <kharigardner@protonmail.com>
2023-03-23 02:42:20 -0400

    Finished base tests, began work on call tests

--- pycallrail/tests/base.py
+++ pycallrail/tests/base.py
@@ -1,30 +1,64 @@
 # Base test to see if the API parent class is working
 
-import unittest
+import unittest as unittest
 import callrail as crl
 from config import api_key
 from api.accounts import Account
-
+from unittest import mock
 class TestBase(unittest.TestCase):
-    def test_base(self):
-        cr = crl.CallRail(api_key=api_key)
-        self.assertIsInstance(cr, crl.CallRail)
+    def setUp(self):
+        self.cr = crl.CallRail(api_key=api_key)
+    
+    def tearDown(self):
+        self.cr = None
 
-# test to see if the api key is not set there is an error
-class TestBaseError(unittest.TestCase):
-    def test_base_error(self):
+    def test_init(self):
+        self.assertIsInstance(self.cr, crl.CallRail)
+
+    def test_init_with_no_api_key(self):
         with self.assertRaises(Exception):
-            cr = crl.CallRail()
+            crl.CallRail()
 
-# test to see if we list accounts when using the list_accounts method
-class TestListAccounts(unittest.TestCase):
-    def test_list_accounts(self):
-        cr = crl.CallRail(api_key=api_key)
-        # i know this should be a single account since the api key is scoped to a single account
-        self.assertIsInstance(cr.list_accounts(), Account)
+    @mock.patch('callrail.CallRail.list_accounts')
+    def test_list_accounts(self, mock_list_accounts):
+        mock_list_accounts.return_value = Account(data=
+        {
+            "id": "ACCfc800e85eba74b62ae02ef58fd76b0db",
+            "name": "Fresh Coat Painters",
+            "numeric_id": 673960154,
+            "outbound_recording_enabled": False,
+            "hipaa_account": False,
+            "agency_in_trial": False,
+            "has_zuora_account": True,
+            "created_at": "2021-03-25 13:34:18 -0400",
+            "brand_status": None
+        },
+        parent=self.cr)
 
-# test to see if we can get an account by id
-class TestGetAccountById(unittest.TestCase):
-    def test_get_account_by_id(self):
-        cr = crl.CallRail(api_key=api_key)
-        self.assertIsInstance(cr.get_account('ACCfc800e85eba74b62ae02ef58fd76b0db'), Account)
+        accounts = self.cr.list_accounts()
+        mock_list_accounts.assert_called_once()
+
+        self.assertIsInstance(accounts, Account)
+        self.assertEqual(accounts.id, "ACCfc800e85eba74b62ae02ef58fd76b0db")
+
+    @mock.patch('callrail.CallRail.get_account')
+    def test_get_account_by_id(self, mock_get_account):
+        mock_get_account.return_value = Account(data=
+        {
+            "id": "ACCfc800e85eba74b62ae02ef58fd76b0db",
+            "name": "Fresh Coat Painters",
+            "numeric_id": 673960154,
+            "outbound_recording_enabled": False,
+            "hipaa_account": False,
+            "agency_in_trial": False,
+            "has_zuora_account": True,
+            "created_at": "2021-03-25 13:34:18 -0400",
+            "brand_status": None
+        },
+        parent=self.cr)
+
+        account = self.cr.get_account("ACCfc800e85eba74b62ae02ef58fd76b0db")
+        mock_get_account.assert_called_once()
+
+        self.assertIsInstance(account, Account)
+        self.assertEqual(account.id, "ACCfc800e85eba74b62ae02ef58fd76b0db")
--- pycallrail/tests/call.py
+++ pycallrail/tests/call.py
@@ -7,14 +7,73 @@ class TestCallAccountMethods(unittest.TestCase):
     """
     Test the various call related methods from the Account class.
     """
-    def test_list_calls(self):
-        cr = crl.CallRail(api_key=api_key)
-        account = cr.get_account('ACCfc800e85eba74b62ae02ef58fd76b0db')
-        callz = account.list_calls()
-        # print a call id so I can use it in the next test
-        print(callz[0].id)
-        self.assertIsInstance(callz, list[Call])
+    def setUp(self):
+        self.cr = crl.CallRail(api_key=api_key)
+        self.account = self.cr.get_account("ACCfc800e85eba74b62ae02ef58fd76b0db")
 
+    def tearDown(self):
+        self.cr = None
+        self.account = None
+
+    def test_init(self):
+        self.assertIsInstance(self.account, crl.Account)
+
+    def test_init_with_no_api_key(self):
+        with self.assertRaises(Exception):
+            crl.CallRail()
+    
+    @mock.patch('callrail.CallRail.list_calls')
+    def test_list_calls(self, mock_list_calls):
+        mock_list_calls.return_value = [
+            Call(data=
+                {
+                    "answered": False,
+                    "business_phone_number": None,
+                    "customer_city": "Denver",
+                    "customer_country": "US",
+                    "customer_name": "RUEGSEGGER SIMO",
+                    "customer_phone_number": "+13036231131",
+                    "customer_state": "CO",
+                    "direction": "inbound",
+                    "duration": 4,
+                    "id": "CAL8154748ae6bd4e278a7cddd38a662f4f",
+                    "recording": "https://api.callrail.com/v3/a/227799611/calls/111222333/recording.json",
+                    "recording_duration": "27",
+                    "recording_player": "https://app.callrail.com/calls/111222333/recording?access_key=3b91eb7f7cc08a4d01ed",
+                    "start_time": "2017-01-24T11:27:48.119-05:00",
+                    "tracking_phone_number": "+13038163491",
+                    "voicemail": False,
+                    "agent_email": "gil@televised.com"
+                }, parent=self.account),
+            Call(data=
+                {
+                    "answered": False,
+                    "business_phone_number": None,
+                    "customer_city": "Denver",
+                    "customer_country": "US",
+                    "customer_name": "RUEGSEGGER SIMO",
+                    "customer_phone_number": "+13036231131",
+                    "customer_state": "CO",
+                    "direction": "inbound",
+                    "duration": 4,
+                    "id": "CAL8154748ae6bd4e278a7cddd38a662f4f",
+                    "recording": "https://api.callrail.com/v3/a/227799611/calls/111222333/recording.json",
+                    "recording_duration": "27",
+                    "recording_player": "aa",
+                    "start_time": "2017-01-24T11:27:48.119-05:00",
+                    "tracking_phone_number": "+13038163491",
+                    "voicemail": False,
+                    "agent_email": "lop@lok.edu"
+                }, parent=self.account)
+        ]
+
+        calls = self.account.list_calls()
+        self.assertEqual(len(calls), 2)
+        self.assertIsInstance(calls[0], crl.Call)
+        self.assertIsInstance(calls[1], crl.Call)
+        self.assertEqual(calls[0].agent_email, "gil@televised.com")
+
+    
     
 
     
